008db32be2911aba37364e0c142813b70499d09e
prakhargoyal106 <52134165+prakhargoyal106@users.noreply.github.com>
2019-07-02 00:30:30 +0530

    Create gradientDescent.m

---
+++ gradientDescent.m
@@ -0,0 +1,18 @@
+
+function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
+
+m = length(y); 
+J_history = zeros(num_iters, 1);
+
+for iter = 1:num_iters
+
+    
+      deviation = (X*theta) - y;
+      temp0 = theta(1)  - (alpha/m)*sum(deviation.*X(:,1));
+      temp1 = theta(2)  - (alpha/m)*sum(deviation.*X(:,2));
+      theta = [temp0 ; temp1];
+      J_history(iter) = computeCost(X, y, theta);
+
+end
+
+end
