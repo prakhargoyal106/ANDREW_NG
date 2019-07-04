14e0a54f35041b2abd218b10f1bb63e106aeeb89
prakhargoyal106 <52134165+prakhargoyal106@users.noreply.github.com>
2019-07-04 17:00:29 +0530

    Update computeCostFunction.m

--- computeCostFunction.m
+++ computeCostFunction.m
@@ -1,4 +1,7 @@
 %PROGRAM FOR CALCULATING COST FUNCTION
+% X is a m x (n+1) matrix, such that  X = [X0 , X1 , X2.........,Xn] ;
+% Xi is a column matrix with m (total number of training example) rows representing ith feature of every training example 
+% theta is a (n+1) x 1 matrix
 function J = computeCost(X, y, theta)
 
 
